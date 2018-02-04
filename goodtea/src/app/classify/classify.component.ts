201c81b3a90038f873344157bf708a34e3528ebc
leson <leson>
2018-02-04 14:49:45 +0800

    11111

--- goodtea/src/app/classify/classify.component.ts
+++ goodtea/src/app/classify/classify.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as mui from "../../assets/js/mui.js"
 @Component({
   selector: 'app-classify',
   templateUrl: './classify.component.html',
@@ -10,6 +10,16 @@ export class ClassifyComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    mui.ready(function() {
+      mui('.mui-scroll-wrapper').scroll({
+          bounce: true,
+          indicators: true,
+          deceleration:mui.os.ios?0.003:0.0009
+      });
+      mui('.mui-scroll').on('tap','.mui-control-item:not(.mui-active)',function(){
+          mui('.mui-slider').slider().gotoItem(this.getAttribute('data-index'));
+      });
+  });
   }
 
 }
